6d43ec67743786fa7d2246c54b1b9f7acccca5b3
akorem <giftnkor@gmail.com>
2022-05-16 15:57:19 +0100

    something

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
+import Section from './Section';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
+      {/* <Main/> */}
+      <Section/>
+
     </div>
   );
 }
--- src/NewData.js
+++ src/NewData.js
@@ -1,6 +1,6 @@
-export const inft = [
-    {id: 1,title: 'Eggys Tech', body: 'We build web apps', author: 'Eggy'}
-    {id: 2,title: 'Sam Tech', body: 'We design sites', author: 'Sam'}
-    {id: 2,title: 'Tech Academy', body: 'We train people', author: 'Tech Studio Academy'}
+export const info = [
+    {id: 1,title: 'Eggys Tech', body: 'We build web apps', author: 'Eggy'},
+    {id: 2,title: 'Sam Tech', body: 'We design sites', author: 'Sam'},
+    {id: 2,title: 'Tech Academy', body: 'We train people', author: 'Tech Studio Academy'},
     {id: 2,title: 'Remas computers', body: 'We procure software apps', author: 'Rema'}
 ]
