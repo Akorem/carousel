838c7a13174656ad343dceb90d312d97f98f3bd2
akorem <giftnkor@gmail.com>
2022-05-16 19:36:57 +0100

    fresh

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Main from './Main';
-import Section from './Section';
 
 function App() {
   return (
     <div className="App">
-      {/* <Main/> */}
-      <Section/>
-
+      <Main/>
     </div>
   );
 }
--- src/NewData.js
+++
@@ -1,6 +0,0 @@
-export const info = [
-    {id: 1,title: 'Eggys Tech', body: 'We build web apps', author: 'Eggy'},
-    {id: 2,title: 'Sam Tech', body: 'We design sites', author: 'Sam'},
-    {id: 2,title: 'Tech Academy', body: 'We train people', author: 'Tech Studio Academy'},
-    {id: 2,title: 'Remas computers', body: 'We procure software apps', author: 'Rema'}
-]
