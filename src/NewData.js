f009edd5c096170c4bee18b3c68c818112ba702c
akorem <giftnkor@gmail.com>
2022-05-16 15:28:19 +0100

    commitment

--- src/NewData.js
+++ src/NewData.js
@@ -1,5 +1,6 @@
 export const inft = [
     {id: 1,title: 'Eggys Tech', body: 'We build web apps', author: 'Eggy'}
-    {id: 2,title: 'Sam Tech', body: 'We design sites', author: 'Eggy'}
-    {id: 1,title: 'Tech Academy', body: 'We train', author: 'Eggy'}
+    {id: 2,title: 'Sam Tech', body: 'We design sites', author: 'Sam'}
+    {id: 2,title: 'Tech Academy', body: 'We train people', author: 'Tech Studio Academy'}
+    {id: 2,title: 'Remas computers', body: 'We procure software apps', author: 'Rema'}
 ]
