29558885d8034140944f0c18f04333859fe3f9d9
akorem <giftnkor@gmail.com>
2022-05-16 15:32:50 +0100

    fresh commit

---
+++ src/Section.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import {info} from './NewData'
+
+const Section = () => {
+    let [details, setDetails] = useState(info)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Section
